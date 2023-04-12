277b4e8321f32cb6ce7b9543c789c1d7fb47ed6e
linzheng <alidlg9045@126.com>
2023-04-12 11:51:28 +0800

    learn: 间接input - object&constant&third-party&class

--- src/case1/index.ts
+++ src/case1/index.ts
@@ -1,4 +1,5 @@
-import { getUserAge, api_getUserAge } from './user';
+import { getUserAge, api_getUserAge, User } from './user';
+import axios from 'axios';
 
 export function doubleUserAge() {
   return getUserAge() * 2
@@ -7,4 +8,31 @@ export function doubleUserAge() {
 export async function api_doubleUserAge() {
   const age = await api_getUserAge()
   return age * 2
+}
+
+// class
+export function getDoubleAgeUseClass_property() {
+  const user = new User()
+  return user.age * 2
+}
+
+export function getDoubleAgeUseClass_fn() {
+  const user = new User()
+  return user.getAge() * 2
+}
+
+interface UserType {
+  name: string
+  age: number
+}
+
+// third-party
+export async function getUserAge_axios() {
+  const user: UserType = await axios('./user-api')
+  return user.age
+}
+
+export async function getUserAge_axiosGet() {
+  const user: UserType = await axios.get('./user-api')
+  return user.age
 }
--- src/case1/user.ts
+++ src/case1/user.ts
@@ -7,6 +7,29 @@ export function api_getUserAge(): Promise<number> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       return resolve(2)
-    },0)
+    }, 0)
   })
 }
+
+// class
+export class User {
+  name: string = 'lin'
+  age: number = 18
+  getAge(): number {
+    return 18
+  }
+}
+
+// object
+export const config = {
+  name: 'lin',
+  age: 18,
+  getAge() {
+    return 18
+  }
+}
+
+// constant
+export const userAge:number = 2
+export const userName:string = 'lin'
+
